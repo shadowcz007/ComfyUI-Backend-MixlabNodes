e5ef8f6ed9bae3154869ee6113e853e8ed9ee59f
shadowcz007 <chizhiwei007@163.com>
2024-04-06 23:02:55 +0800

    优化目录

--- run_nvidia_gpu.bat
+++ run_nvidia_gpu.bat
@@ -1,2 +1,2 @@
-C:\Users\38957\Documents\ai-lab\ComfyUI_windows_portable\python_embeded\python.exe -s ComfyUI\main.py --windows-standalone-build --lowvram --port 8188 --listen 0.0.0.0 --multi-user --enable-cors-header
+C:\Users\38957\Documents\ai-lab\ComfyUI_windows_portable\python_embeded\python.exe -s ComfyUI\main.py --extra-model-paths-config ./extra_model_paths.yaml --windows-standalone-build --lowvram --port 8188 --listen 0.0.0.0 --multi-user --enable-cors-header
 pause

7b729c7f39056b46a5c2c42556a4aaea4c506b6a
shadowcz007 <chizhiwei007@163.com>
2024-05-07 21:00:37 +0800

    update

--- install_win.bat
+++ install_win.bat
@@ -1,6 +1,5 @@
 @echo off
 
-
 setlocal enabledelayedexpansion
 set "file=.\config\python_path.txt"
 set "python_exec="
@@ -9,23 +8,17 @@ set "python_exec=!python_exec! %%i"
 )
 echo %python_exec%
 
-
-
 set "requirements_txt=%~dp0\config\requirements.txt"
 @REM set "python_exec=.\python_embeded\python.exe"
 
 echo Installing ComfyUI's Mixlab Nodes..
 
-if exist "%python_exec%" (
+if exist %python_exec% (
     echo Installing with ComfyUI Portable
     for /f "delims=" %%i in (%requirements_txt%) do (
         %python_exec% -s -m pip install "%%i" -i https://pypi.tuna.tsinghua.edu.cn/simple
     )
-) else (
-    echo Installing with system Python
-    for /f "delims=" %%i in (%requirements_txt%) do (
-        pip install "%%i" -i https://pypi.tuna.tsinghua.edu.cn/simple
-    )
 )
+
 
 pause

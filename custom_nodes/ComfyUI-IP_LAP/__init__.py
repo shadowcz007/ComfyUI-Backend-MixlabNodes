a165f771d89b9d2441468b4f3dbd0bb9a35dd50e
shadowcz007 <chizhiwei007@163.com>
2024-07-31 12:12:06 +0800

    update

--- custom_nodes/ComfyUI-IP_LAP/__init__.py
+++
@@ -1,8 +0,0 @@
-from .nodes import IP_LAP
-
-NODE_CLASS_MAPPINGS = {
-    "IP_LAP": IP_LAP
-}
-NODE_DISPLAY_NAME_MAPPINGS = {
-    "IP_LAP": "IP_LAP Node", 
-}
--- custom_nodes/comfyui-ultralytics-yolo/__init__.py
+++
@@ -1,8 +0,0 @@
-from .nodes.detect import detectNode
-
-NODE_CLASS_MAPPINGS = {
-    "DetectByLabel": detectNode
-}
-NODE_DISPLAY_NAME_MAPPINGS = {
-    "DetectByLabel": "Detect By Label"
-}

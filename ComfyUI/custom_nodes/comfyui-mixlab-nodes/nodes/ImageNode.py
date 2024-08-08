dc67f1f2202048eaf33e1005c3d4b07250a16a73
shadowcz007 <chizhiwei007@163.com>
2024-08-08 21:58:18 +0800

    1

--- ComfyUI/custom_nodes/comfyui-mixlab-nodes/nodes/ImageNode.py
+++ ComfyUI/custom_nodes/comfyui-mixlab-nodes/nodes/ImageNode.py
@@ -3301,7 +3301,11 @@ class DepthViewer_:
             "required": {
                 "image": ("IMAGE",),
                 "depth_map": ("IMAGE",),
-            }
+
+            },
+             "optional":{
+                  "frames":("IMAGEBASE64",), 
+                },
         }
 
     def __init__(self):
@@ -3313,11 +3317,17 @@ class DepthViewer_:
             folder_paths.get_output_directory())
 
 
-    RETURN_TYPES = ()
+    RETURN_TYPES = ("IMAGE",)
+    RETURN_NAMES = ("frames",)
+    
     OUTPUT_NODE = True
+
+    INPUT_IS_LIST = False
+    OUTPUT_IS_LIST = (False,)
+
     FUNCTION = "run"
-    CATEGORY = "♾️Mixlab/Image"
-    def run(self, image, depth_map):
+    CATEGORY = "♾️Mixlab/3D"
+    def run(self, image, depth_map,frames=None):
         self.saved_reference.clear()
         self.saved_depth.clear()
         image = image[0].detach().cpu().numpy()
@@ -3326,9 +3336,9 @@ class DepthViewer_:
         image = Image.fromarray(np.clip(255. * image, 0, 255).astype(np.uint8)).convert('RGB')
         depth = Image.fromarray(np.clip(255. * depth, 0, 255).astype(np.uint8))
 
-        return self.display([image], [depth])
+        return self.display([image], [depth],frames)
 
-    def display(self, reference_image, depth_map):
+    def display(self, reference_image, depth_map,frames):
         for (batch_number, (single_image, single_depth)) in enumerate(zip(reference_image, depth_map)):
             filename_with_batch_num = self.filename.replace("%batch_num%", str(batch_number))
 
@@ -3351,4 +3361,25 @@ class DepthViewer_:
             })
             self.counter += 1
 
-        return {"ui": {"reference_image": self.saved_reference, "depth_map": self.saved_depth}}
+
+        ims=[]
+        image1 = Image.new('RGB', (512, 512), color='black')
+        image1=pil2tensor(image1)
+
+        if frames!=None:
+            for im in frames['images']:
+                # print(im)
+                if 'type' in im and (not f"[{im['type']}]" in im['name']):
+                    im['name']=im['name']+" "+f"[{im['type']}]"
+                    
+                output_image, output_mask = load_image_to_tensor(im['name'])
+                ims.append(output_image)
+
+            if len(ims)>0:
+                image1 = ims[0]
+                for image2 in ims[1:]:
+                    if image1.shape[1:] != image2.shape[1:]:
+                        image2 = comfy.utils.common_upscale(image2.movedim(-1, 1), image1.shape[2], image1.shape[1], "bilinear", "center").movedim(1, -1)
+                    image1 = torch.cat((image1, image2), dim=0)
+
+        return {"ui": {"reference_image": self.saved_reference, "depth_map": self.saved_depth}, "result": (image1,)}

c188c32c7408928320889372f3cffe3c4081107a
shadowcz007 <chizhiwei007@163.com>
2024-08-08 16:58:11 +0800

    update

--- ComfyUI/custom_nodes/comfyui-mixlab-nodes/nodes/ImageNode.py
+++ ComfyUI/custom_nodes/comfyui-mixlab-nodes/nodes/ImageNode.py
@@ -3291,3 +3291,65 @@ class ImageListToBatch_:
         out = torch.cat(out, dim=0)
 
         return (out,)
+
+
+# https://github.com/gokayfem/ComfyUI-Depth-Visualization?tab=readme-ov-file
+class DepthViewer_:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "image": ("IMAGE",),
+                "depth_map": ("IMAGE",),
+            }
+        }
+
+    def __init__(self):
+        self.saved_reference = []
+        self.saved_depth = []
+        self.full_output_folder, 
+        self.filename, 
+        self.counter, 
+        self.subfolder, self.filename_prefix = folder_paths.get_save_image_path(
+            "imagesave", 
+            folder_paths.get_output_directory())
+
+    RETURN_TYPES = ()
+    OUTPUT_NODE = True
+    FUNCTION = "run"
+    CATEGORY = "♾️Mixlab/Image"
+    def run(self, image, depth_map):
+        self.saved_reference.clear()
+        self.saved_depth.clear()
+        image = image[0].detach().cpu().numpy()
+        depth = depth_map[0].detach().cpu().numpy()
+
+        image = Image.fromarray(np.clip(255. * image, 0, 255).astype(np.uint8)).convert('RGB')
+        depth = Image.fromarray(np.clip(255. * depth, 0, 255).astype(np.uint8))
+
+        return self.display([image], [depth])
+
+    def display(self, reference_image, depth_map):
+        for (batch_number, (single_image, single_depth)) in enumerate(zip(reference_image, depth_map)):
+            filename_with_batch_num = self.filename.replace("%batch_num%", str(batch_number))
+
+            image_file = f"{filename_with_batch_num}_{self.counter:05}_reference.png"
+            single_image.save(path.join(self.full_output_folder, image_file))
+
+            depth_file = f"{filename_with_batch_num}_{self.counter:05}_depth.png"
+            single_depth.save(path.join(self.full_output_folder, depth_file))
+
+            self.saved_reference.append({
+                "filename": image_file,
+                "subfolder": self.subfolder,
+                "type": "output"
+            })
+
+            self.saved_depth.append({
+                "filename": depth_file,
+                "subfolder": self.subfolder,
+                "type": "output"
+            })
+            self.counter += 1
+
+        return {"ui": {"reference_image": self.saved_reference, "depth_map": self.saved_depth}}

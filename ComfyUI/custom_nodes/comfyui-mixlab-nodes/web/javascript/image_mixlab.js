3390ba1d9430b032e2af45e7ed95013f725e07e8
shadowcz007 <chizhiwei007@163.com>
2024-08-30 18:48:57 +0800

    update

--- ComfyUI/custom_nodes/comfyui-mixlab-nodes/web/javascript/image_mixlab.js
+++ ComfyUI/custom_nodes/comfyui-mixlab-nodes/web/javascript/image_mixlab.js
@@ -349,7 +349,7 @@ app.registerExtension({
           draw (ctx, node, widget_width, y, widget_height) {
             Object.assign(
               this.div.style,
-              get_position_style(ctx, widget_width, 44, node.size[1], 36)
+              get_position_style(ctx, widget_width, 44, node.size[1], 60)
             )
           }
         }
@@ -733,10 +733,10 @@ app.registerExtension({
           type: 'div',
           name: 'image_base64',
           draw (ctx, node, widget_width, y, widget_height) {
-            Object.assign(
-              this.div.style,
-              get_position_style(ctx, widget_width, 44, node.size[1], 44)
-            )
+            Object.assign(this.div.style, {
+              ...get_position_style(ctx, widget_width, y, node.size[1], 72),
+              top: `${widget_height}px`
+            })
           },
           serialize: false
         }
@@ -857,7 +857,7 @@ app.registerExtension({
       let imagePreview = node.widgets.filter(w => w.name == 'image_base64')[0]
       // console.log('#LoadImagesToBatch', imagesWidget.value?.base64)
       let imagesDiv = imagePreview.div.querySelector('.images_preview')
-
+      imagesDiv.innerHTML = ''
       for (const d of imagesWidget.value?.base64 || []) {
         let im = createInputImageForBatch(d, imagesWidget)
         imagesDiv.appendChild(im)
@@ -873,7 +873,7 @@ app.registerExtension({
         let imagePreview = node.widgets.filter(w => w.name == 'image_base64')[0]
 
         let imagesDiv = imagePreview?.div?.querySelector('.images_preview')
-
+        imagesDiv.innerHTML = ''
         for (const d of imagesWidget.value?.base64 || []) {
           let im = createInputImageForBatch(d, imagesWidget)
           imagesDiv.appendChild(im)

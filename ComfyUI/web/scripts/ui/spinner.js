5478549c53be0e5aeddb2da8f1cc6d6baa4d340b
shadowcz007 <chizhiwei007@163.com>
2024-09-07 11:14:47 +0800

    1

--- ComfyUI/web/scripts/ui/spinner.js
+++ ComfyUI/web/scripts/ui/spinner.js
@@ -1,2 +1,9 @@
-// Shim for scripts\ui\spinner.ts
-export const createSpinner = window.comfyAPI.spinner.createSpinner;
+import { addStylesheet } from "../utils.js";
+
+addStylesheet(import.meta.url);
+
+export function createSpinner() {
+	const div = document.createElement("div");
+	div.innerHTML = `<div class="lds-ring"><div></div><div></div><div></div><div></div></div>`;
+	return div.firstElementChild;
+}
